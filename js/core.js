04d3463bc33efa722d2635479e6bc2d70f6f7c40
Ultra Developer <80384998+UltraSoftware0@users.noreply.github.com>
2021-06-07 19:44:25 +0300

    New Function: ucfirst
    Uppercase the first character of a string.

--- js/core.js
+++ js/core.js
@@ -1,10 +1,19 @@
 // START: Sleep
 function sleep(milliseconds) // Reference: https://sitepoint.com/delay-sleep-pause-wait
 {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-      currentDate = Date.now();
-  } while (currentDate - date < milliseconds); 
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds); 
 }
 // END: Sleep
+
+
+// START: Initial Uppercase
+function ucfirst (string) {
+    if (typeof(string) != 'string') return;
+
+    return string[0].toUpperCase() + string.substring(1);
+}
+// END: Initial Uppercase
