628607c2fb61129ddff20ec7a7e620fb6732c059
Ultra Developer <80384998+UltraSoftware0@users.noreply.github.com>
2021-06-09 23:17:55 +0300

    New Functions: "jsonClone" & "dynamicRows"

--- js/core.js
+++ js/core.js
@@ -5,15 +5,79 @@ function sleep(milliseconds) // Reference: https://sitepoint.com/delay-sleep-pau
     let currentDate = null;
     do {
         currentDate = Date.now();
-    } while (currentDate - date < milliseconds); 
+    } while (currentDate - date < milliseconds);
 }
 // END: Sleep
 
 
 // START: Initial Uppercase
-function ucfirst (string) {
-    if (typeof(string) != 'string') return;
+function ucfirst(string) {
+    if (typeof (string) != 'string') return;
 
     return string[0].toUpperCase() + string.substring(1);
 }
 // END: Initial Uppercase
+
+
+// START: Clone Json
+function jsonClone(obj) {
+    return JSON.parse(JSON.stringify(obj)); // Reference: https://stackoverflow.com/a/5344074
+}
+// END: Clone Json
+
+
+// START: Add/Remove Row
+$.fn.dynamicRows = function (data) {
+    var main = $(this);
+
+    main.find('.add-row').on('click', function (e) {
+        e.preventDefault();
+        addRow();
+    });
+
+    // Add Row
+    function addRow(_data = {}) {
+        var row_id = Date.now();
+        var htmlToAdd = data.htmlToAdd;
+
+        var variables = jsonClone(_data.variables ?? {});
+
+        Object.entries(variables).forEach(([key, value]) => {
+            var replaceThis = '%' + key + '%';
+            htmlToAdd = htmlToAdd.replace(new RegExp(replaceThis, 'g'), value);
+        });
+        htmlToAdd = htmlToAdd.replace(/\%.+?\%/g, '');
+
+        var elemToAdd;
+        if ($(htmlToAdd).prop("tagName") == undefined) {
+            elemToAdd = $('<span></span>').append(htmlToAdd);
+        } else {
+            elemToAdd = $(htmlToAdd);
+        }
+
+        elemToAdd.attr('dynamic-rows-id', row_id);
+        elemToAdd.addClass('dynamicRows-row');
+        elemToAdd.find('.remove-row').attr('dynamic-rows-id', row_id);
+
+        main.append(elemToAdd);
+
+        // START: Remove Button
+        elemToAdd.find('.remove-row').on('click', function () {
+            var dynamicRows_id = $(this).attr('dynamic-rows-id');
+
+            $('.dynamicRows-row[dynamic-rows-id="' + dynamicRows_id + '"]').remove();
+        });
+        // END: Remove Button
+    }
+
+    // Remove All Rows
+    function removeAllRows() {
+        main.find('.dynamicRows-row').remove();
+    }
+
+    return {
+        addRow: addRow,
+        removeAllRows: removeAllRows
+    };
+};
+// END: Add/Remove Row
